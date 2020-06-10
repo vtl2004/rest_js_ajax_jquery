22c0dd0fd50909cfabe2fc370e346863cfff900a
Vadim Karpov <ksvkvv1990@gmail.com>
2020-06-11 01:30:38 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserRestController.java
@@ -0,0 +1,84 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import web.model.Role;
+import web.model.User;
+import web.service.RoleService;
+import web.service.UserService;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/admin/users")
+public class UserRestController {
+
+    private UserService userService;
+    private RoleService roleService;
+
+    @Autowired
+    public UserRestController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @PostMapping
+    public User addUser(User user, @RequestParam(name = "role") String[] roles){
+        Set<Role> rolSet = new HashSet<>();
+        for (String strRole : roles) {
+            Role role = roleService.findByRole(strRole);
+            rolSet.add(role);
+        }
+        user.setRoles(rolSet);
+        return userService.addUser(user);
+    }
+
+    @PutMapping
+    public User updateUser(@ModelAttribute User user, @RequestParam(name = "role") String[] roles){
+        Set<Role> rolSett = new HashSet<>();
+        for (String strRole : roles) {
+            Role role = roleService.findByRole(strRole);
+            rolSett.add(role);
+        }
+        user.setRoles(rolSett);
+        return userService.updateUser(user);
+    }
+
+    @GetMapping("delete/{id}")
+    public  void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id){
+
+        return userService.getById(id);
+    }
+
+    @GetMapping("/roles")
+    public List<Role> getAllRoles(){
+
+        return roleService.findAllRoles();
+    }
+
+    @GetMapping("/new/{username}{password}")
+    public User getUser(@PathVariable String username, @PathVariable String password){
+        return userService.getByUsernameAndPasswordService(username, password);
+    }
+
+
+
+}
